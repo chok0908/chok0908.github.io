209ceb6968f03768d0fcdba997a101a8c57d4223
chan <daiasccho@gmail.com>
2020-01-21 13:24:04 +0900

    add ie check

--- js/chunk-2d0bd643.9456e7e5.js
+++ js/chunk-2d0bd643.9456e7e5.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0bd643"],{"2c95":function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("v-container",{attrs:{fluid:""}},[n("v-col",{attrs:{"text-xs-center":"",wrap:""}},[t._v(" Test2 ")])],1)},c=[],s={data:function(){return{}},created:function(){this.$store.dispatch("changeTitle","test2"),this.$store.dispatch("changeSubject","test2")}},r=s,i=n("2877"),o=n("6544"),u=n.n(o),l=n("62ad"),d=n("a523"),h=Object(i["a"])(r,a,c,!1,null,null,null);e["default"]=h.exports;u()(h,{VCol:l["a"],VContainer:d["a"]})}}]);
-//# sourceMappingURL=chunk-2d0bd643.39a19c84.js.map
+//# sourceMappingURL=chunk-2d0bd643.9456e7e5.js.map
